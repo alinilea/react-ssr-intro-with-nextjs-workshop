1616cc9174ad9e8dc7242d9ef8fcf22e5e857dc3
Alin Ilea <ilea.alin@yahoo.com>
2019-05-27 01:26:16 +0300

    added styling example using the Next's built-in CSS support: styled-jsx

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import Head from 'next/head';
 
-function IndexPage() {
+function Index() {
   return (
     <Fragment>
       <Head>
@@ -10,9 +10,21 @@ function IndexPage() {
         <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon.ico" />
       </Head>
 
-      <h2>Welcome to React SSR intro with Next.js</h2>
+      <h2>Welcome to React SSR intro with Next</h2>
+
+      <style jsx>{`
+        h2 {
+          color: #222;
+        }
+      `}</style>
+      <style jsx global>{`
+        h2 {
+          font-style: italic;
+          letter-spacing: 1.5px;
+        }
+      `}</style>
     </Fragment>
   );
 }
 
-export default IndexPage;
+export default Index;
